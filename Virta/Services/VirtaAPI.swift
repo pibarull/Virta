b6479ac8e40b1641ede817b219cdd47e612a9fe8
Ilia Ershov <il998x@gmail.com>
2022-03-01 17:38:03 +0200

    Services: Add VirtaAPI
    `VirtaAPI` is a scope for different types of `VirtaAPIEndpoint`.

---
+++ Virta/Services/VirtaAPI.swift
@@ -0,0 +1,20 @@
+//
+//  VirtaAPI.swift
+//  Virta
+//
+//  Created by Ilia Ershov on 01.03.2022.
+//
+
+/// `VirtaAPI` is a scope for different types of `VirtaAPIEndpoint`.
+///
+/// Example:
+///
+///     Auth        -> "/auth"
+
+enum VirtaAPI {
+
+    static func setAuthData(_ authModelParameters: AuthModelParameters)
+    -> VirtaAPIEndpoint<AuthModelParameters, AuthModel> {
+        return .init(path: "/auth", method: .post, requestData: authModelParameters)
+    }
+}
